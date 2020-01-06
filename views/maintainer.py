4a01fa6a0ef9425d89017258dff78b5d88c248b9
Arpit Gupta <guptarpit1997@gmail.com>
2020-01-07 03:21:06 +0530

    optimize search by removing unnecessary querying of all data

--- views/maintainer.py
+++ views/maintainer.py
@@ -1,6 +1,8 @@
 import swapper
 from rest_framework import filters, permissions, viewsets
 
+from django.db.models import Q
+
 from formula_one.enums.active_status import ActiveStatus
 from omniport.utils import switcher
 
@@ -18,28 +20,25 @@ class MaintainerViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Maintainer.objects_filter(ActiveStatus.IS_ACTIVE).all()
     serializer_class = MaintainerSerializer
 
-    filter_backends = [filters.SearchFilter, ]
-    search_fields = [
-        # Self
-        'role',
-        'designation',
-        'post',
-
-        # Person
-        'person__short_name',
-        'person__full_name',
-
-        # User
-        'person__user__username',
-
-        # Student
-        'person__student__enrolment_number',
-
-        # Contact information
-        'person__contact_information__primary_phone_number',
-        'person__contact_information__secondary_phone_number',
-        'person__contact_information__email_address',
-        'person__contact_information__institute_webmail_address',
-    ]
-
     pagination_class = None
+
+    def get_queryset(self):
+        """
+        :return: 10 maintainer search objects
+        """
+
+        query = self.request.query_params.get('search', None)
+        maintainer = Maintainer.objects_filter(ActiveStatus.IS_ACTIVE).filter(
+            Q(role__icontains=query) |
+            Q(designation__icontains=query) |
+            Q(post__icontains=query) |
+            Q(person__short_name__icontains=query) |
+            Q(person__full_name__icontains=query) |
+            Q(person__user__username__icontains=query) |
+            Q(person__student__enrolment_number__icontains=query) |
+            Q(person__contact_information__primary_phone_number__icontains=query) |
+            Q(person__contact_information__secondary_phone_number__icontains=query) |
+            Q(person__contact_information__email_address__icontains=query) |
+            Q(person__contact_information__institute_webmail_address__icontains=query)
+        )
+        return maintainer[:10]
--- views/student.py
+++ views/student.py
@@ -1,7 +1,11 @@
 import swapper
-from rest_framework import filters, permissions, viewsets
+
+from rest_framework import permissions, viewsets
+
+from django.db.models import Q
 
 from formula_one.enums.active_status import ActiveStatus
+
 from omniport.utils import switcher
 
 Student = swapper.load_model('kernel', 'Student')
@@ -15,26 +19,23 @@ class StudentViewSet(viewsets.ReadOnlyModelViewSet):
 
     permission_classes = [permissions.IsAuthenticated, ]
 
-    queryset = Student.objects_filter(ActiveStatus.IS_ACTIVE).all()
     serializer_class = StudentSerializer
-
-    filter_backends = [filters.SearchFilter, ]
-    search_fields = [
-        # Self
-        'enrolment_number',
-
-        # Person
-        'person__short_name',
-        'person__full_name',
-
-        # User
-        'person__user__username',
-
-        # Contact information
-        'person__contact_information__primary_phone_number',
-        'person__contact_information__secondary_phone_number',
-        'person__contact_information__email_address',
-        'person__contact_information__institute_webmail_address',
-    ]
-
     pagination_class = None
+
+    def get_queryset(self):
+        """
+        :return: ten student search objects
+        """
+
+        query = self.request.query_params.get('search', None)
+        student = Student.objects_filter(ActiveStatus.IS_ACTIVE).filter(
+            Q(enrolment_number__icontains=query) |
+            Q(person__short_name__icontains=query) |
+            Q(person__full_name__icontains=query) |
+            Q(person__user__username__icontains=query) |
+            Q(person__contact_information__primary_phone_number__icontains=query) |
+            Q(person__contact_information__secondary_phone_number__icontains=query) |
+            Q(person__contact_information__email_address__icontains=query) |
+            Q(person__contact_information__institute_webmail_address__icontains=query)
+        )
+        return student[:10]
