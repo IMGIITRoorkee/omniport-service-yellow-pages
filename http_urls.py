8b717c347c35789353af740e684b8f70f375eb6e
pooja allampallewar <poojasa718@gmail.com>
2022-09-22 17:56:30 +0530

    Add a filter to filter syudents on basis of branch, year, degree and branch-year

--- http_urls.py
+++ http_urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 from yellow_pages.views.maintainer import MaintainerViewSet
 from yellow_pages.views.person import PersonViewSet
 from yellow_pages.views.student import StudentViewSet
+from yellow_pages.views.filter import FilterViewSet
 
 app_name = 'yellow_pages'
 
@@ -11,6 +12,7 @@ router = routers.SimpleRouter()
 router.register('person', PersonViewSet, basename='person')
 router.register('maintainer', MaintainerViewSet, basename='maintainer')
 router.register('student', StudentViewSet, basename='student')
+router.register('filter', FilterViewSet, basename='filter')
 
 urlpatterns = [
     path('', include(router.urls)),
