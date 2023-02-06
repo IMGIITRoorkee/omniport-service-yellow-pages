934fc1db5129fb7320f0f4024cb963d5a56666ad
pooja allampallewar <poojasa718@gmail.com>
2023-02-07 01:54:28 +0530

    Remove extra imports

--- views/student.py
+++ views/student.py
@@ -1,8 +1,6 @@
 import swapper
 
 from rest_framework import permissions, viewsets
-from rest_framework.response import Response
-from rest_framework import status
 
 from django.db.models import Q
 
